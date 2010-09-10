0dea325c7393e0943818d59dcdf2981250b0fa69
Ariya Hidayat <ariya.hidayat@gmail.com>
2010-09-09 23:47:10 -0700

    jsedit: Minimalistic editing widget for JavaScript code.

---
+++ javascript/jsedit/jsedit.cpp
@@ -0,0 +1,510 @@
+/*
+  This file is part of the Ofi Labs X2 project.
+
+  Copyright (C) 2010 Ariya Hidayat <ariya.hidayat@gmail.com>
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "jsedit.h"
+
+#include <QtGui>
+
+class JSHighlighter : public QSyntaxHighlighter
+{
+public:
+    JSHighlighter(QTextDocument *parent = 0);
+    void setColor(JSEdit::ColorComponent component, const QColor &color);
+
+protected:
+    void highlightBlock(const QString &text);
+
+private:
+    QSet<QString> m_keywords;
+    QSet<QString> m_knownIds;
+    QHash<JSEdit::ColorComponent, QColor> m_colors;
+};
+
+JSHighlighter::JSHighlighter(QTextDocument *parent)
+    : QSyntaxHighlighter(parent)
+{
+    // default color scheme
+    m_colors[JSEdit::Normal]     = QColor("#000000");
+    m_colors[JSEdit::Comment]    = QColor("#808080");
+    m_colors[JSEdit::Number]     = QColor("#008000");
+    m_colors[JSEdit::String]     = QColor("#800000");
+    m_colors[JSEdit::Operator]   = QColor("#808000");
+    m_colors[JSEdit::Identifier] = QColor("#000020");
+    m_colors[JSEdit::Keyword]    = QColor("#000080");
+    m_colors[JSEdit::BuiltIn]    = QColor("#008080");
+
+    // https://developer.mozilla.org/en/JavaScript/Reference/Reserved_Words
+    m_keywords << "break";
+    m_keywords << "case";
+    m_keywords << "catch";
+    m_keywords << "continue";
+    m_keywords << "default";
+    m_keywords << "delete";
+    m_keywords << "do";
+    m_keywords << "else";
+    m_keywords << "finally";
+    m_keywords << "for";
+    m_keywords << "function";
+    m_keywords << "if";
+    m_keywords << "in";
+    m_keywords << "instanceof";
+    m_keywords << "new";
+    m_keywords << "return";
+    m_keywords << "switch";
+    m_keywords << "this";
+    m_keywords << "throw";
+    m_keywords << "try";
+    m_keywords << "typeof";
+    m_keywords << "var";
+    m_keywords << "void";
+    m_keywords << "while";
+    m_keywords << "with";
+
+    m_keywords << "true";
+    m_keywords << "false";
+    m_keywords << "null";
+
+    // built-in and other popular objects + properties
+    m_knownIds << "Object";
+    m_knownIds << "prototype";
+    m_knownIds << "create";
+    m_knownIds << "defineProperty";
+    m_knownIds << "defineProperties";
+    m_knownIds << "getOwnPropertyDescriptor";
+    m_knownIds << "keys";
+    m_knownIds << "getOwnPropertyNames";
+    m_knownIds << "constructor";
+    m_knownIds << "__parent__";
+    m_knownIds << "__proto__";
+    m_knownIds << "__defineGetter__";
+    m_knownIds << "__defineSetter__";
+    m_knownIds << "eval";
+    m_knownIds << "hasOwnProperty";
+    m_knownIds << "isPrototypeOf";
+    m_knownIds << "__lookupGetter__";
+    m_knownIds << "__lookupSetter__";
+    m_knownIds << "__noSuchMethod__";
+    m_knownIds << "propertyIsEnumerable";
+    m_knownIds << "toSource";
+    m_knownIds << "toLocaleString";
+    m_knownIds << "toString";
+    m_knownIds << "unwatch";
+    m_knownIds << "valueOf";
+    m_knownIds << "watch";
+
+    m_knownIds << "Function";
+    m_knownIds << "arguments";
+    m_knownIds << "arity";
+    m_knownIds << "caller";
+    m_knownIds << "constructor";
+    m_knownIds << "length";
+    m_knownIds << "name";
+    m_knownIds << "apply";
+    m_knownIds << "bind";
+    m_knownIds << "call";
+
+    m_knownIds << "String";
+    m_knownIds << "fromCharCode";
+    m_knownIds << "length";
+    m_knownIds << "charAt";
+    m_knownIds << "charCodeAt";
+    m_knownIds << "concat";
+    m_knownIds << "indexOf";
+    m_knownIds << "lastIndexOf";
+    m_knownIds << "localCompare";
+    m_knownIds << "match";
+    m_knownIds << "quote";
+    m_knownIds << "replace";
+    m_knownIds << "search";
+    m_knownIds << "slice";
+    m_knownIds << "split";
+    m_knownIds << "substr";
+    m_knownIds << "substring";
+    m_knownIds << "toLocaleLowerCase";
+    m_knownIds << "toLocaleUpperCase";
+    m_knownIds << "toLowerCase";
+    m_knownIds << "toUpperCase";
+    m_knownIds << "trim";
+    m_knownIds << "trimLeft";
+    m_knownIds << "trimRight";
+
+    m_knownIds << "Array";
+    m_knownIds << "isArray";
+    m_knownIds << "index";
+    m_knownIds << "input";
+    m_knownIds << "pop";
+    m_knownIds << "push";
+    m_knownIds << "reverse";
+    m_knownIds << "shift";
+    m_knownIds << "sort";
+    m_knownIds << "splice";
+    m_knownIds << "unshift";
+    m_knownIds << "concat";
+    m_knownIds << "join";
+    m_knownIds << "filter";
+    m_knownIds << "forEach";
+    m_knownIds << "every";
+    m_knownIds << "map";
+    m_knownIds << "some";
+    m_knownIds << "reduce";
+    m_knownIds << "reduceRight";
+
+    m_knownIds << "RegExp";
+    m_knownIds << "global";
+    m_knownIds << "ignoreCase";
+    m_knownIds << "lastIndex";
+    m_knownIds << "multiline";
+    m_knownIds << "source";
+    m_knownIds << "exec";
+    m_knownIds << "test";
+
+    m_knownIds << "JSON";
+    m_knownIds << "parse";
+    m_knownIds << "stringify";
+
+    m_knownIds << "decodeURI";
+    m_knownIds << "decodeURIComponent";
+    m_knownIds << "encodeURI";
+    m_knownIds << "encodeURIComponent";
+    m_knownIds << "eval";
+    m_knownIds << "isFinite";
+    m_knownIds << "isNaN";
+    m_knownIds << "parseFloat";
+    m_knownIds << "parseInt";
+    m_knownIds << "Infinity";
+    m_knownIds << "Math";
+    m_knownIds << "NaN";
+    m_knownIds << "undefined";
+
+    m_knownIds << "document";
+    m_knownIds << "window";
+    m_knownIds << "navigator";
+    m_knownIds << "userAgent";
+}
+
+void JSHighlighter::setColor(JSEdit::ColorComponent component, const QColor &color)
+{
+    m_colors[component] = color;
+    rehighlight();
+}
+
+void JSHighlighter::highlightBlock(const QString &text)
+{
+    // parsing state
+    enum {
+        Start = -1,
+        Number = 1,
+        Identifier = 2,
+        String = 3,
+        Comment = 4,
+        Regex = 5
+    };
+
+    int state = previousBlockState();
+    int start = 0;
+    int i = 0;
+    while (i < text.length()) {
+        QChar ch = text.at(i);
+        QChar next = (i < text.length() - 1) ? text.at(i + 1) : QChar();
+
+        switch (state) {
+
+        case Start:
+            start = i;
+            if (ch.isSpace()) {
+                ++i;
+            } else if (ch.isDigit()) {
+                ++i;
+                state = Number;
+            } else if (ch.isLetter() || ch == '_') {
+                ++i;
+                state = Identifier;
+            } else if (ch == '\'' || ch == '\"') {
+                ++i;
+                state = String;
+            } else if (ch == '/' && next == '*') {
+                ++i;
+                ++i;
+                state = Comment;
+            } else if (ch == '/' && next == '/') {
+                i = text.length();
+                setFormat(start, text.length(), m_colors[JSEdit::Comment]);
+            } else if (ch == '/' && next != '*') {
+                ++i;
+                state = Regex;
+            } else {
+                if (!QString("(){}[]").contains(ch))
+                    setFormat(start, 1, m_colors[JSEdit::Operator]);
+                ++i;
+                state = Start;
+            }
+            break;
+
+        case Number:
+            if (ch.isSpace() || !ch.isDigit()) {
+                setFormat(start, i - start, m_colors[JSEdit::Number]);
+                state = Start;
+            } else {
+                ++i;
+            }
+            break;
+
+        case Identifier:
+            if (ch.isSpace() || !(ch.isDigit() || ch.isLetter() || ch == '_')) {
+                QString token = text.mid(start, i - start).trimmed();
+                if (m_keywords.contains(token))
+                    setFormat(start, i - start, m_colors[JSEdit::Keyword]);
+                else if (m_knownIds.contains(token))
+                    setFormat(start, i - start, m_colors[JSEdit::BuiltIn]);
+                state = Start;
+            } else {
+                ++i;
+            }
+            break;
+
+        case String:
+            if (ch == text.at(start)) {
+                QChar prev = (i > 0) ? text.at(i - 1) : QChar();
+                if (prev != '\\') {
+                    ++i;
+                    setFormat(start, i - start, m_colors[JSEdit::String]);
+                    state = Start;
+                } else {
+                    ++i;
+                }
+            } else {
+                ++i;
+            }
+            break;
+
+        case Comment:
+            if (ch == '*' && next == '/') {
+                ++i;
+                ++i;
+                setFormat(start, i - start, m_colors[JSEdit::Comment]);
+                state = Start;
+            } else {
+                ++i;
+            }
+            break;
+
+        case Regex:
+            if (ch == '/') {
+                QChar prev = (i > 0) ? text.at(i - 1) : QChar();
+                if (prev != '\\') {
+                    ++i;
+                    setFormat(start, i - start, m_colors[JSEdit::String]);
+                    state = Start;
+                } else {
+                    ++i;
+                }
+            } else {
+                ++i;
+            }
+            break;
+
+        default:
+            state = Start;
+            break;
+        }
+    }
+
+    if (state == Comment)
+        setFormat(start, text.length(), m_colors[JSEdit::Comment]);
+    else
+        state = Start;
+
+    setCurrentBlockState(state);
+}
+
+
+struct BlockInfo {
+    int position;
+    int number;
+};
+
+Q_DECLARE_TYPEINFO(BlockInfo, Q_PRIMITIVE_TYPE);
+
+class SidebarWidget : public QWidget
+{
+public:
+    SidebarWidget(JSEdit *editor);
+    void paintEvent(QPaintEvent *event);
+    QVector<BlockInfo> lineNumbers;
+    QColor backgroundColor;
+    QColor lineNumberColor;
+    QFont font;
+};
+
+SidebarWidget::SidebarWidget(JSEdit *editor)
+    : QWidget(editor)
+{
+    backgroundColor = Qt::lightGray;
+    lineNumberColor = Qt::black;
+}
+
+void SidebarWidget::paintEvent(QPaintEvent *event)
+{
+    QPainter p(this);
+    p.fillRect(event->rect(), backgroundColor);
+    p.setPen(lineNumberColor);
+    p.setFont(font);
+    int fh = QFontMetrics(font).height();
+    foreach(BlockInfo ln, lineNumbers)
+        p.drawText(0, ln.position, width() - 4, fh, Qt::AlignRight, QString::number(ln.number));
+}
+
+class JSEditPrivate
+{
+public:
+    JSEdit *editor;
+    JSHighlighter *highlighter;
+    SidebarWidget *sidebar;
+    QColor cursorColor;
+};
+
+JSEdit::JSEdit(QWidget *parent)
+    : QPlainTextEdit(parent)
+    , d_ptr(new JSEditPrivate)
+{
+    d_ptr->editor = this;
+    d_ptr->highlighter = new JSHighlighter(document());
+    d_ptr->sidebar = new SidebarWidget(this);
+    d_ptr->cursorColor = QColor(255, 255, 192);
+
+    connect(this, SIGNAL(cursorPositionChanged()), this, SLOT(updateCursor()));
+    connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateSidebar()));
+    connect(this, SIGNAL(updateRequest(QRect, int)), this, SLOT(updateSidebar(QRect, int)));
+
+#ifdef Q_OS_MAC
+    QFont textFont = font();
+    textFont.setPointSize(12);
+    textFont.setFamily("Monaco");
+    setFont(textFont);
+#elif Q_OS_UNIX
+    QFont textFont = font();
+    textFont.setFamily("Monospace");
+    setFont(textFont);
+#endif
+}
+
+JSEdit::~JSEdit()
+{
+}
+
+void JSEdit::setColor(ColorComponent component, const QColor &color)
+{
+    Q_D(JSEdit);
+
+    if (component == Background) {
+        QPalette pal = palette();
+        pal.setColor(QPalette::Base, color);
+        setPalette(pal);
+    } else if (component == Normal) {
+        QPalette pal = palette();
+        pal.setColor(QPalette::Text, color);
+        setPalette(pal);
+    } else if (component == Sidebar) {
+        d->sidebar->backgroundColor = color;
+        updateSidebar();
+    } else if (component == LineNumber) {
+        d->sidebar->lineNumberColor = color;
+        updateSidebar();
+    } else if (component == Cursor) {
+        d->cursorColor = color;
+        updateCursor();
+    } else {
+        d->highlighter->setColor(component, color);
+    }
+}
+
+void JSEdit::resizeEvent(QResizeEvent *e)
+{
+    QPlainTextEdit::resizeEvent(e);
+    updateSidebar();
+}
+
+void JSEdit::updateCursor()
+{
+    QTextEdit::ExtraSelection highlight;
+    highlight.format.setBackground(d_ptr->cursorColor);
+    highlight.format.setProperty(QTextFormat::FullWidthSelection, true);
+    highlight.cursor = textCursor();
+    highlight.cursor.clearSelection();
+
+    QList<QTextEdit::ExtraSelection> extraSelections;
+    extraSelections.append(highlight);
+    setExtraSelections(extraSelections);
+}
+
+void JSEdit::updateSidebar(const QRect &rect, int d)
+{
+    Q_UNUSED(rect)
+    if (d != 0)
+        updateSidebar();
+}
+
+void JSEdit::updateSidebar()
+{
+    Q_D(JSEdit);
+
+    d->sidebar->font = this->font();
+
+    int digits = 1;
+    int maxLines = blockCount();
+    for (int number = 10; number < maxLines; number *= 10)
+        ++digits;
+    int sw = 4 + fontMetrics().width('w') * digits + 4;
+    setViewportMargins(sw, 0, 0, 0);
+
+    d->sidebar->setGeometry(0, 0, sw, height());
+    QRectF sidebarRect(0, 0, sw, height());
+
+    QVector<BlockInfo> lineNumbers;
+    lineNumbers.reserve(d->sidebar->lineNumbers.count());
+    QTextBlock block = firstVisibleBlock();
+    while (block.isValid()) {
+        if (block.isVisible()) {
+            QRectF rect = blockBoundingGeometry(block).translated(contentOffset());
+            if (sidebarRect.intersects(rect)) {
+                BlockInfo ln;
+                ln.position = rect.top();
+                ln.number = block.blockNumber() + 1;
+                lineNumbers += ln;
+            }
+            if (rect.top() > sidebarRect.bottom())
+                break;
+        }
+        block = block.next();
+    }
+
+    d->sidebar->lineNumbers = lineNumbers;
+    d->sidebar->update();
+}
+
