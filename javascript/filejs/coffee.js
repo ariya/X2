25c2580f65a380952fa09ee731dadb04e056a5ea
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-01-06 00:01:06 -0800

    filejs: Add the example script to drive CoffeeScript compiler.

---
+++ javascript/filejs/coffee.js
@@ -0,0 +1,51 @@
+// Drive the CoffeeScript compiler to convert CoffeeScript to JavaScript
+
+// Important: full path to the compiler
+var compiler = '/home/yourusername/bin/coffee-script.js';
+
+if (system.args.length !== 2) {
+    system.print("Usage: coffee.js inputfile");
+    system.exit(-1);
+}
+
+if (!fs.exists(compiler)) {
+    system.print("CoffeeScript compiler " + compiler + " is not available!");
+    system.exit(-1);
+}
+
+var loadFile = function(fname)
+{
+    var content = '',
+        f;
+
+    if (fs.exists(fname)) {
+        try {
+            f = fs.open(fname, 'r');
+            while (true) {
+                content += f.next();
+                content += '\n';
+            }
+        }
+        catch (e) { } // EOF exception
+        finally {
+            f.close();
+        }
+    }
+
+    return content;
+};
+
+eval(loadFile(compiler));
+if (typeof this.CoffeeScript !== 'object' || typeof this.CoffeeScript.compile !== 'function') {
+    system.print("Something is wrong with the CoffeeScript compiler (" + compiler + ").");
+    system.print("Hint: make sure you reformat it, e.g. using jsbeautifier.org");
+    system.exit(-1);
+}
+
+var input = loadFile(system.args[1]);
+if (input.length === 0) {
+    system.print('Nothing to convert!');
+    system.exit(1);
+}
+
+system.print(this.CoffeeScript.compile(input));
